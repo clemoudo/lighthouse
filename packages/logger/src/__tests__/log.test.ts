95988f633d2c247d3aae1b5865f814c218e9a613
Clément <184501524+clemoudo@users.noreply.github.com>
2026-05-01 08:06:31 +0200

    fix(infra): test coverage and build var

--- packages/logger/src/__tests__/log.test.ts
+++ packages/logger/src/__tests__/log.test.ts
@@ -1,11 +1,32 @@
-import { describe, it, expect, jest } from "@jest/globals"
-import { log } from ".."
-
-jest.spyOn(global.console, "log")
+import { describe, it, expect, jest, beforeEach, afterEach } from "@jest/globals"
+import { log, logger } from ".."
 
 describe("@repo/logger", () => {
-  it("prints a message", () => {
+  beforeEach(() => {
+    jest.spyOn(console, "info").mockImplementation(() => {})
+    jest.spyOn(console, "warn").mockImplementation(() => {})
+    jest.spyOn(console, "error").mockImplementation(() => {})
+    jest.spyOn(console, "debug").mockImplementation(() => {})
+  })
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  it("prints a message (old)", () => {
     log("hello")
-    expect(console.log).toHaveBeenCalled()
+    expect(console.info).toHaveBeenCalled()
+  })
+  it("prints a message (info)", () => {
+    logger.info("hello")
+    expect(console.info).toHaveBeenCalled()
+  })
+  it("prints a message (warn)", () => {
+    logger.warn("hello")
+    expect(console.warn).toHaveBeenCalled()
+  })
+  it("prints a message (error)", () => {
+    logger.error("hello")
+    expect(console.error).toHaveBeenCalled()
   })
 })
